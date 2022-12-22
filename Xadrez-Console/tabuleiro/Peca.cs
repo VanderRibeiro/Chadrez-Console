3240025a05923f3e57f3ca39b523721dad7b136b
Vander Ribeiro <vbruno383@gmail.com>
2022-12-22 04:34:40 -0300

    Testes iniciais com movimentos possiveis das pecas (com erro)

--- Xadrez-Console/tabuleiro/Peca.cs
+++ Xadrez-Console/tabuleiro/Peca.cs
@@ -1,6 +1,6 @@
 ﻿namespace tabuleiro
 {
-    class Peca{
+    abstract class Peca{
 
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -15,9 +15,12 @@
             this.qteMovimentos = 0;
         }
 
-        public void incrementarQteMovimentos(){
-            qteMovimentos++;
+        public void incrementarQteMovimentos()
+        {
+            this.qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();//vai ser abstract pq não tem a peça específica, eventualmente a classe também vai ser abstract
 
     }
 }
--- Xadrez-Console/xadrez/Rei.cs
+++ Xadrez-Console/xadrez/Rei.cs
@@ -9,5 +9,65 @@ namespace xadrez{
         public override string ToString(){
             return "R";
         }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != this.cor;
+        }
+        public override bool[,] movimentosPossiveis(){
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima do rei
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //nordeste do rei
+            pos.definirValores(posicao.linha - 1, posicao.coluna +1);//acima do rei
+            if (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //direita do rei
+            pos.definirValores(posicao.linha, posicao.coluna + 1);//acima do rei
+            if (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //sudeste do rei
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //abaixo do rei
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //sudoeste do rei
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //esquerda do rei
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //noroeste do rei
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna] = true;
+            }
+            return mat;
+        }
     }
 }
